5990220e40dc4baca9711788d70719bbe0b0218a
Kawã Sousa <kawasousacontato@gmail.com>
2025-07-09 15:50:52 -0300

    :bug: fix: Remove o uso do index.html na raiz do projeto

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    root: 'src',
-    build: {
-        outDir: '../dist',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/pages/home/index.html'),
+        error: resolve(__dirname, 'src/pages/error/index.html'),
+      },
     },
+  },
 });
